c43d259d73bfd15283232cfd54ccf67f311224c1
MANN TOMAR <manntomar46@gmail.com>
2025-08-16 16:16:23 +0530

    Add files via upload

---
+++ CHAPTER 3/LEARN WITH CODE/escape sequence/ESCAPE_SEQUENCE/src/Main.java
@@ -0,0 +1,13 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        System.out.print("Hello and welcome!\n");
+        System.out.print("Hello and welcome!\n\t");
+        System.out.print("\"Hello and welcome!\n");
+        System.out.print(" \bHello and welcome!\n");
+        System.out.print("\'Hello\' and welcome!\n");
+        System.out.print("\\Hello and welcome!\\");
+
+    }
+}
---
+++ CHAPTER_2/LEARN WITH CODE/FIRST FILE/FIRST_FILE.java
@@ -0,0 +1,10 @@
+import java.lang.*;
+
+public class FIRST_FILE {
+  public static void main(String[] args) {
+    System.out.print("WELCOME ");
+    System.out.print("TO MANN's ");
+    System.out.println("FIRST PROGRAM");
+    System.out.print("YOU ARE MY RESPONSIBILITY TO TEACH YOU JAVA");
+}
+}
